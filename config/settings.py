a9568af06d01aaf496f633b890bdfebbd0604508
Sebastiao-Domingos <domingossebastiao380@gmail.com>
2026-04-28 10:43:47 +0100

    verify

--- config/settings.py
+++ config/settings.py
@@ -153,7 +153,7 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
